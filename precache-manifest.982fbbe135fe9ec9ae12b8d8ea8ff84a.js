5e5ac1524e8d201caef0853d73898d724476e4aa
unknown <wabo225@uky.edu>
2019-02-24 20:33:20 -0500

    Updates

---
+++ precache-manifest.982fbbe135fe9ec9ae12b8d8ea8ff84a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6667446d7c25ea808912128ec199fba",
+    "url": "/FOOD/static/media/pic (1).f6667446.jpg"
+  },
+  {
+    "revision": "0bf241d146635be0d2ce",
+    "url": "/FOOD/static/css/main.7c1c5650.chunk.css"
+  },
+  {
+    "revision": "28096bfcf3077499e9c0",
+    "url": "/FOOD/static/js/1.28096bfc.chunk.js"
+  },
+  {
+    "revision": "a9c0bda81a959f26186c",
+    "url": "/FOOD/static/js/runtime~main.a9c0bda8.js"
+  },
+  {
+    "revision": "ca2024c0514a4e24f72ddff2a228666a",
+    "url": "/FOOD/static/media/Code.ca2024c0.PNG"
+  },
+  {
+    "revision": "44941d5f1f37563f89d7966dda923ea7",
+    "url": "/FOOD/static/media/biscuit.44941d5f.jpeg"
+  },
+  {
+    "revision": "3064b0a223d4a34abf32a76da4d1af9b",
+    "url": "/FOOD/static/media/dinnerRolls.3064b0a2.jpg"
+  },
+  {
+    "revision": "0bf241d146635be0d2ce",
+    "url": "/FOOD/static/js/main.0bf241d1.chunk.js"
+  },
+  {
+    "revision": "a2968af7523d319106fd3fd6ada0f7dc",
+    "url": "/FOOD/static/media/pic (2).a2968af7.jpg"
+  },
+  {
+    "revision": "c7afefeb3138bf6ad2c2e0c8852be3bd",
+    "url": "/FOOD/static/media/pic (3).c7afefeb.jpg"
+  },
+  {
+    "revision": "84b1874ed94d0b5d08a1c17a4c064e37",
+    "url": "/FOOD/static/media/pic (4).84b1874e.jpg"
+  },
+  {
+    "revision": "e9e9440c7a6c1b4cecfb7371b59d9877",
+    "url": "/FOOD/static/media/pic (5).e9e9440c.jpg"
+  },
+  {
+    "revision": "da79552a29299c0095a2fa1a3df2494b",
+    "url": "/FOOD/static/media/pic (6).da79552a.jpg"
+  },
+  {
+    "revision": "d529feff79e27c0e0be38810c52d7a7c",
+    "url": "/FOOD/static/media/pic (7).d529feff.jpg"
+  },
+  {
+    "revision": "31e6c5dd634965e11b2ff188b313e764",
+    "url": "/FOOD/index.html"
+  }
+];
