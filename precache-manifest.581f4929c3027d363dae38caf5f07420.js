d35975486e31cf9fbe82110baed1b988387e5f91
unknown <wabo225@uky.edu>
2019-02-24 20:23:23 -0500

    first build

---
+++ precache-manifest.581f4929c3027d363dae38caf5f07420.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6667446d7c25ea808912128ec199fba",
+    "url": "/static/media/pic (1).f6667446.jpg"
+  },
+  {
+    "revision": "ee053b2d1c3051c14573",
+    "url": "/static/css/main.7c1c5650.chunk.css"
+  },
+  {
+    "revision": "28096bfcf3077499e9c0",
+    "url": "/static/js/1.28096bfc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ca2024c0514a4e24f72ddff2a228666a",
+    "url": "/static/media/Code.ca2024c0.PNG"
+  },
+  {
+    "revision": "44941d5f1f37563f89d7966dda923ea7",
+    "url": "/static/media/biscuit.44941d5f.jpeg"
+  },
+  {
+    "revision": "3064b0a223d4a34abf32a76da4d1af9b",
+    "url": "/static/media/dinnerRolls.3064b0a2.jpg"
+  },
+  {
+    "revision": "ee053b2d1c3051c14573",
+    "url": "/static/js/main.ee053b2d.chunk.js"
+  },
+  {
+    "revision": "a2968af7523d319106fd3fd6ada0f7dc",
+    "url": "/static/media/pic (2).a2968af7.jpg"
+  },
+  {
+    "revision": "c7afefeb3138bf6ad2c2e0c8852be3bd",
+    "url": "/static/media/pic (3).c7afefeb.jpg"
+  },
+  {
+    "revision": "84b1874ed94d0b5d08a1c17a4c064e37",
+    "url": "/static/media/pic (4).84b1874e.jpg"
+  },
+  {
+    "revision": "e9e9440c7a6c1b4cecfb7371b59d9877",
+    "url": "/static/media/pic (5).e9e9440c.jpg"
+  },
+  {
+    "revision": "da79552a29299c0095a2fa1a3df2494b",
+    "url": "/static/media/pic (6).da79552a.jpg"
+  },
+  {
+    "revision": "d529feff79e27c0e0be38810c52d7a7c",
+    "url": "/static/media/pic (7).d529feff.jpg"
+  },
+  {
+    "revision": "f5cabe4c4a3c194d8740863eeabc76e1",
+    "url": "/index.html"
+  }
+];
